3802e284c0d92f4c9cefe6211403317f52564f5f
Andrey Pechennikov <ap@devandy.me>
2021-02-17 12:49:46 +0600

    add component post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, PostComponent]
 })
 export class AppModule { }
