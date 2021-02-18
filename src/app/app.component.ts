12454e97da6fba0e4816bce74ae0047010644c4b
Andrey Pechennikov <ap@devandy.me>
2021-02-18 18:32:16 +0600

    test bind function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+
+  title = 'Dynamic title'
+  img = 'https://banner2.cleanpng.com/20180720/zia/kisspng-react-javascript-library-web-development-vue-js-funding-icon-5b51604fbf7995.0841849115320597277843.jpg'
+  inputValue = ''
+
+  constructor() {
+    setTimeout(() => {
+      this.img = 'https://banner2.cleanpng.com/20180424/kxw/kisspng-angularjs-web-application-single-page-application-original-sticker-5adf7b95b14632.9610094315245956057261.jpg'
+    }, 5000)
+  }
+
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
+  }
+  
 }
