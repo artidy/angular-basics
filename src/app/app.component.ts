ff618e39294c5df9f4444f18bfd2d357ce0ce475
Andrey Pechennikov <ap@devandy.me>
2021-02-20 16:59:22 +0600

    using pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dynamic title';
-  toggle = false;
-  arr = [3, 5, 8, 13]
-  objs = [
-    {title: 'Post 1', author: 'Andrey', comments: [
-      {name: 'Max', text: 'lorem1'},
-      {name: 'Natasha', text: 'lorem2'}
-    ]},
-    {title: 'Post 2', author: 'Andrey 2', comments: [
-      {name: 'Max 2', text: 'lorem1'},
-      {name: 'Natasha 2', text: 'lorem2'}
-    ]}
-  ]
+  now: Date = new Date();
   
 }
