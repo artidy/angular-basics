13d86ce159345e51d14b18f4850a8179903d2fdb
Andrey Pechennikov <ap@devandy.me>
2021-02-20 11:56:17 +0600

    bind ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  title = 'Dynamic title'
-  img = 'https://banner2.cleanpng.com/20180720/zia/kisspng-react-javascript-library-web-development-vue-js-funding-icon-5b51604fbf7995.0841849115320597277843.jpg'
-  inputValue = 'initial'
-
-  constructor() {
-    setTimeout(() => {
-      this.img = 'https://banner2.cleanpng.com/20180424/kxw/kisspng-angularjs-web-application-single-page-application-original-sticker-5adf7b95b14632.9610094315245956057261.jpg'
-    }, 5000)
-  }
-
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str;
-  }
+  title = 'Dynamic title';
+  backgroundToggle = false;
   
 }
