87116fe7203899765f073134a2d90645423f782f
Andrey Pechennikov <ap@devandy.me>
2021-02-21 12:37:23 +0600

    add filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,18 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Аляска', text: 'Самое лучшее пиво в мире'},
+    {title: 'Холупень', text: 'Самый лучший хлеб в мире'},
+    {title: 'JavaScript', text: 'Самый лучший язык программирования в мире'}
+  ]
+  search = ''
+
+  addPost() {
+    this.posts.unshift({
+      title: 'Today day',
+      text: 'Today day is perfect'
+    })
+  }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MultByPipe } from './pipes/mult-by.pipe';
 import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MultByPipe,
-    ExMarksPipe
+    ExMarksPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
