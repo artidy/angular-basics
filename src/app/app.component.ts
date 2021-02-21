abc621e425475e8a3f0dd7ca90fd676eefda62e0
Andrey Pechennikov <ap@devandy.me>
2021-02-21 14:50:08 +0600

    using services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,38 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-
-export interface Post {
-  title: string,
-  text: string
-}
+import { AppCounterService } from './services/app-counter.service';
+import { LocalCounterService } from './services/local-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    LocalCounterService
+  ]
 })
 export class AppComponent {
-  promise: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise resolved')
-    }, 5000)
-  })
-  date: Observable<Date> = new Observable(obs => {
-    setInterval(() => {
-      obs.next(new Date())
-    }, 1000)
-  })
+
+  private counter: number = this.localCounterSevice.counter
+
+  constructor(public appCounterService: AppCounterService,
+              private localCounterSevice: LocalCounterService) { }
+
+  increase() {
+    this.localCounterSevice.inc()
+    this.setCounter()
+  }
+
+  decrease() {
+    this.localCounterSevice.dec()
+    this.setCounter()
+  }
+
+  private setCounter() {
+    this.counter = this.localCounterSevice.counter
+  }
+
+  getCounter() {
+    return this.counter
+  }
   
 }
