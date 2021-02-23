fc6ed327ac4dde5eecbb92ec835da5238b1a45f0
Andrey Pechennikov <ap@devandy.me>
2021-02-23 18:41:42 +0600

    using router, guard and resolver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,11 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Todo, TodosService } from './services/todos.service';
-
+import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  todos: Todo[] = []
-  todoTitle = ''
-  loading = false
-  error = ''
-
-  constructor(private todoService: TodosService) {}
-
-  ngOnInit() {
-    this.fetchTodos()
-  }
-
-  addTodo() {
-    if (this.todoTitle.trim()) {
-      const newTodo: Todo = {
-        title: this.todoTitle,
-        completed: false
-      }  
-      this.todoService.addTodo(newTodo)
-        .subscribe(
-          todo => {
-            this.todos.unshift(todo)
-            this.todoTitle = ''
-          },
-          error => this.error = `Ошибка: ${error.message}`
-        )
-    }
-  }
-
-  fetchTodos() {
-    this.loading = true
-      this.todoService.getTodos()
-      .subscribe(
-        todos => {
-          this.todos = todos
-          this.loading = false
-        },
-        error => this.error = `Ошибка: ${error.message}`
-      )
-  }
-
-  removeTodo(id: number) {
-    this.todoService.removeTodo(id)
-      .subscribe(
-        () => {
-          this.todos = this.todos.filter(item => item.id !== id)
-        },
-        error => this.error = `Ошибка: ${error.message}`
-      )
-  }
-
-  completeTodo(id: number) {
-    this.todoService.completeTodo(id)
-      .subscribe(
-        todo => {
-          this.todos.find(item => item.id === todo.id).completed = true
-        },
-        error => this.error = `Ошибка: ${error.message}`
-      )  
-  }
+export class AppComponent {
+  constructor(public authService: AuthService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
-import { NgModule, Provider } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
-
-const INTERSEPTOR_PROVIDER: Provider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: AuthInterceptor,
-  multi: true
-}
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    AboutExtraComponent,
+    PostComponent,
+    PostsComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [INTERSEPTOR_PROVIDER],
+  providers: [],
   bootstrap: [
     AppComponent
   ]
